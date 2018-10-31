ce5149496b67b259efe8e1f56e09f88b5d403e23
Emilio Molinari <17145424+xates@users.noreply.github.com>
2018-10-31 11:40:43 +0100

    Minor changes to api.js

--- api.js
+++ api.js
@@ -4,4 +4,4 @@ const PORT = process.env.PORT || 3000
  
 app.get('/', (req, res) => res.send('Hello :)'))
 
-app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
+app.listen(PORT, () => console.log('Server listening on port ' + PORT))
