1f51e3fb46339b41925933e47e76fc637f38bdac
Emilio Molinari <xates97@gmail.com>
2018-11-19 18:58:18 +0100

    Minor changes to style across files (whitespaces, newlines, ...)

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
 const express = require('express')
+
 const app = express()
 const PORT = process.env.PORT || 3000
- 
+
 app.get('/', (req, res) => res.send('Hello :)'))
 
 app.listen(PORT, () => console.log('Server listening on port ' + PORT))
