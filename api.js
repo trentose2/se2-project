14e7002b5bd8c910f3c1ee22e774624de46911fa
Jasin Atipi <jasin.atipi@gmail.com>
2018-11-26 15:11:02 +0100

    change file structure, add jest

--- api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.get('/', (req, res) => res.send('Hello :)'))
-
-app.listen(PORT, () => console.log('Server listening on port ' + PORT))
