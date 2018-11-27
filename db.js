75bde51a26d196244393e3cc27e0ec34e7ab4f94
Emilio Molinari <xates97@gmail.com>
2018-11-27 16:23:12 +0100

    Add basic db

---
+++ db.js
@@ -0,0 +1,10 @@
+exports = module.exports = {};
+
+exports.users = new Array();
+exports.groups = new Array();
+exports.tasks = new Array();
+exports.pools = new Array();
+exports.exams = new Array();
+exports.papers = new Array();
+exports.submissions = new Array();
+exports.reviews = new Array();
