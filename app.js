d2ff9ae2ee2d63ff64749c953633bebaa05cc419
Emilio Molinari <xates97@gmail.com>
2018-11-27 16:23:02 +0100

    Create POST implementation for /paths

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const groups = require('./groups/groups.js')
 
+const pools = require('./paths/pools.js');
+
 app.get('/', (req, res) => {
     res.json({msg: 'Hello :)' });
 });
 
 app.post('/v1/groups', groups.doPost);
+
+app.post('/v1/pools', pools.doPost);
 
 module.exports = app;
