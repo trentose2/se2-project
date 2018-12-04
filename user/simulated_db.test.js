37525859986d7ed7795881e51dda0a5c99e281f9
alexzanetti <alex.zanetti-1@studenti.unitn.it>
2018-12-04 22:47:45 +0100

    Add PUT method and test cases

--- user/simulated_db.test.js
+++ user/simulated_db.test.js
@@ -4,7 +4,7 @@ const db = require('./simulated_db.js');
 
 test('POST on /user to insert of user', () =>
     expect(
-        db.insertUser(new user.User('e','u'))
+        db.insertUser(new user.User('e','u','f','l'))
     ).toBe(true)
 );
 test('POST on /user not working insert of object', () =>
@@ -14,7 +14,7 @@ test('POST on /user not working insert of object', () =>
 );
 test('POST on /user to insert of user', () =>
     expect(
-        db.insertUser(new user.User('e','u'))
+        db.insertUser(new user.User('e','u','f','l'))
     ).toBe(true)
 );
 test('GET on /users/{id} with proper id', () =>
@@ -30,6 +30,16 @@ test('GET on /users/{id} not working with non integer id', () =>
 test('GET on /users/{id} not working with non-existing id', () =>
     expect(
         db.getUserById(420000)
+    ).toBe(null)
+);
+//test('PUT on /users/{id} not working with existing id', () =>
+//    expect(
+//        db.updateUser(0,'New email','New username','New firstName','New lastName')
+//    ).toBe(user.User)
+//);
+test('PUT on /users/{id} not working with non-existing id', () =>
+    expect(
+        db.updateUser(87897,'New email','New username','New firstName','New lastName')
     ).toBe(null)
 );
 test('DELETE on /users/{id} working with proper id', ()=>{
@@ -49,7 +59,7 @@ test('DELETE on /users/{id} with non existing id', ()=>{
     ).toBe(1);
 });
 test('PUT on /users/{id} to update user information: should return updated user', () =>{
-    u=new user.User("e1","u1");
+    u=new user.User('e1','u1','f1','l1');
     db.insertUser(u);
     id=u.getId();
     u.setEmail("email_new");
--- user/user_class.test.js
+++ user/user_class.test.js
@@ -3,19 +3,57 @@ const user=require('./user_class.js')
 
 test('user creation', () =>
     expect(
-        new user.User("a@b.com","c").constructor
+        new user.User("a@b.com","c", "ambo", "pippo").constructor
     ).toBe(user.User)
 );
-test('user email creation went ok', () =>
+test('user email creation went ok: get email', () =>
     expect(
-        new user.User('email','username').getEmail()
+        new user.User('email','username', 'firstName', 'lastName').getEmail()
     ).toBe('email')
 );
-test('user username creation went ok', () =>
+test('user username creation went ok: get username', () =>
     expect(
-        new user.User('email','username').getUsername()
+        new user.User('email','username', 'firstName', 'lastName').getUsername()
     ).toBe('username')
 );
+test('user username creation went ok: get firstName', () =>
+    expect(
+        new user.User('email','username', 'firstName', 'lastName').getFirstName()
+    ).toBe('firstName')
+);
+test('user username creation went ok: get lastName', () =>
+    expect(
+        new user.User('email','username', 'firstName', 'lastName').getLastName()
+    ).toBe('lastName')
+);
+test('user email creation went ok: set email', () => {
+    u = new user.User('email','username', 'firstName', 'lastName');
+    u.setEmail('New email');
+    expect(
+        u.getEmail()
+    ).toBe('New email')
+});
+test('user email creation went ok: set username', () => {
+    u = new user.User('email','username', 'firstName', 'lastName');
+    u.setUsername('New username');
+    expect(
+        u.getUsername()
+    ).toBe('New username')
+});
+test('user email creation went ok: set firstName', () => {
+    u = new user.User('email','username', 'firstName', 'lastName');
+    u.setFirstName('New firstName');
+    expect(
+        u.getFirstName()
+    ).toBe('New firstName')
+});
+test('user email creation went ok: set lastName', () => {
+    u = new user.User('email','username', 'firstName', 'lastName');
+    u.setLastName('New lastName');
+    expect(
+        u.getLastName()
+    ).toBe('New lastName')
+});
 test('id working', ()=>
     expect(
         new user.User('t','a').getId() - new user.User('t','a').getId()
